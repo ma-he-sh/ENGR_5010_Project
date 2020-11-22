597a8de1ac62ce11f122971dfc0349877cca26d1
ma-he-sh <mi6softlab@gmail.com>
2020-11-22 14:42:48 -0500

    --git fix--

--- cacheinit.py
+++
@@ -1,6 +0,0 @@
-from functions.funct import createInitCache, saveCSV, GetEnv
-import numpy as np
-
-if __name__ == "__main__":
-    list = createInitCache()
-    saveCSV( GetEnv('CACHE_DATA'), list)
